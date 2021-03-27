b598c60ee6e0cda38f078ad291a540887a26a754
Artem Bulakh <artem.bulakh1@gmail.com>
2021-03-27 12:02:26 +0300

    delete all

--- src/SQL/stg_db/DDL/tables/create_movies_table.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS stg_movies;
-CREATE TABLE stg_movies(
-	movie_id INT UNSIGNED NOT NULL,
-    title VARCHAR(256),
-    genres_str VARCHAR(256),
-    CONSTRAINT pk_stg_movies PRIMARY KEY(movie_id)
-);
